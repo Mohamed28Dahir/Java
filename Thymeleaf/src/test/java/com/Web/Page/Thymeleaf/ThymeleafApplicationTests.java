ca115fb37f5a717f971f55fd0f2b3521831cb41f
Mohamed28Dahir <mohameddahirosman26@gmail.com>
2025-05-01 19:02:23 +0300

    Add files via upload

---
+++ Thymeleaf/src/test/java/com/Web/Page/Thymeleaf/ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Web.Page.Thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
